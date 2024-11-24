c56a6aaa1caec0189e769a08d8049bfcc8ef7079
Davud Magomedov <davmagbek54321@gmail.com>
2024-11-24 12:03:36 +0400

    make AIL lazy

--- src/storage/available_id_list.rs
+++ src/storage/available_id_list.rs
@@ -1,9 +1,10 @@
 use crate::error_ext::ComResult;
 use crate::types::ID;
 
-use std::fs::OpenOptions;
+use std::fs::{OpenOptions, File};
 use std::io::Write;
 use std::path::{Path, PathBuf};
+use std::cell::OnceCell;
 
 use memmap2::MmapMut;
 
@@ -44,17 +45,29 @@ pub struct AvailableIDList {
     path: PathBuf,
     // Markup:
     // [blocks]
-    mapped: MmapMut,
+    mapped: OnceCell<MmapMut>,
+}
+
+fn open_file(path: &Path) -> ComResult<File> {
+    Ok(OpenOptions::new().read(true).write(true).open(path)?)
+}
+
+fn create_new_file(path: &Path) -> ComResult<File> {
+    Ok(OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create_new(true)
+        .open(&path)?)
 }
 
 impl AvailableIDList {
     pub fn open(working_dir: &Path) -> ComResult<Self> {
+        //let file = OpenOptions::new().read(true).write(true).open(&path)?;
+        //unsafe { MmapMut::map_mut(&file)? }
         let path = working_dir.join(AIL_FILE);
-        let file = OpenOptions::new().read(true).write(true).open(&path)?;
-
         Ok(AvailableIDList {
             path,
-            mapped: unsafe { MmapMut::map_mut(&file)? },
+            mapped: OnceCell::new(),
         })
     }
 
@@ -68,42 +81,46 @@ impl AvailableIDList {
 
         file.write_all(&[0; BLOCK_SIZE])?;
 
+        let mapped = OnceCell::new();
+        let _ = mapped.set(unsafe { MmapMut::map_mut(&file)? });
+
         Ok(AvailableIDList {
             path,
-            mapped: unsafe { MmapMut::map_mut(&file)? },
+            mapped,
         })
     }
 
     pub fn grab_id(&mut self) -> ComResult<ID> {
-        let (byte_index, byte) = match self
-            .byte_section()
+        let byte_section = self.byte_section()?;
+        let (byte_index, byte) = match byte_section
             .into_iter()
             .enumerate()
             .find(|(_, &byte)| byte != u8::MAX)
         {
             None => {
-                let index = self.byte_section().len();
+                let index = byte_section.len();
                 self.grow()?;
 
-                (index, self.mapped[index])
+                (index, self.get_mapped()?[index])
             }
             Some((index, &byte)) => (index, byte),
         };
 
         let index_in_byte = find_zero_in_byte(byte);
-        self.byte_section_mut()[byte_index] = set_one_in_byte(byte, index_in_byte);
+        self.byte_section_mut()?[byte_index] = set_one_in_byte(byte, index_in_byte);
 
         return Ok(ID::new((8 * byte_index + index_in_byte) as u64));
     }
 
     pub fn release_id(&mut self, id: ID) -> ComResult<()> {
         // 8 - count of bits in a byte.
-        if id.value() < (self.byte_count() * 8) as u64 {
+        if id.value() < (self.byte_count()? * 8) as u64 {
+            let byte_section_mut = self.byte_section_mut()?;
             let byte_index = id.value() / 8;
             let index_in_byte = id.value() % 8;
 
-            self.byte_section_mut()[byte_index as usize] = set_zero_in_byte(
-                self.byte_section()[byte_index as usize],
+            byte_section_mut[byte_index as usize] = set_zero_in_byte(
+                byte_section_mut[byte_index as usize],
                 index_in_byte as usize,
             );
         }
@@ -111,7 +128,28 @@ impl AvailableIDList {
         Ok(())
     }
 
+    fn get_mapped(&self) -> ComResult<&MmapMut> {
+        match self.mapped.get() {
+            Some(mapped) => Ok(mapped),
+            None => {
+                let _ = self.mapped.set(unsafe { MmapMut::map_mut(&open_file(&self.path)?)? });
+                Ok(self.mapped.get().unwrap())
+            }
+        }
+    }
+
+    fn get_mapped_mut(&mut self) -> ComResult<&mut MmapMut> {
+        if self.mapped.get().is_some() {
+            Ok(self.mapped.get_mut().unwrap())
+        } else {
+            let _ = self.mapped.set(unsafe { MmapMut::map_mut(&open_file(&self.path)?)? });
+            Ok(self.mapped.get_mut().unwrap())
+        }
+    }
+
     fn grow(&mut self) -> ComResult<()> {
+        let _ = self.mapped.take();
+
         let mut file = OpenOptions::new()
             .read(true)
             .write(true)
@@ -121,20 +159,18 @@ impl AvailableIDList {
         file.write_all(&[0; BLOCK_SIZE])?;
         file.flush()?;
 
-        self.mapped = unsafe { MmapMut::map_mut(&file)? };
-
         Ok(())
     }
 
-    fn byte_section(&self) -> &[u8] {
-        &self.mapped[..]
+    fn byte_section(&self) -> ComResult<&[u8]> {
+        Ok(&self.get_mapped()?[..])
     }
 
-    fn byte_section_mut(&mut self) -> &mut [u8] {
-        &mut self.mapped[..]
+    fn byte_section_mut(&mut self) -> ComResult<&mut [u8]> {
+        Ok(&mut self.get_mapped_mut()?[..])
     }
 
-    fn byte_count(&self) -> usize {
-        self.mapped.len()
+    fn byte_count(&self) -> ComResult<usize> {
+        Ok(self.get_mapped()?.len())
     }
 }
